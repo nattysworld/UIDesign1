ca6672ef66332adeb2bf7c852790e2c06ce33bc3
nattysworld <162095996+nattysworld@users.noreply.github.com>
2024-05-30 09:56:05 +0900

    darkmode/website

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+ document.querySelector('#darkmode').addEventListener('click', function() {
+      document.getElementsByTagName('body')[0].classList.toggle('dark');
+});
+// document.querySelector
+
+
+// body.darkmode {
+// 	background: black;
+// 	color: white;
+// }
