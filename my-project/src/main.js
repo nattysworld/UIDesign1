4062772d27adb784d696475cf7969af251cdf05f
nattysworld <162095996+nattysworld@users.noreply.github.com>
2024-06-13 20:02:49 +0900

    import three

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,5 +1,6 @@
 
-import * as THREE from '../node_modules/three/build/three.module.js';
+// import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { ImprovedNoise } from '../node_modules/three/examples/jsm/math/ImprovedNoise.js';
 // import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.142.0/examples/jsm/controls/OrbitControls.js';
